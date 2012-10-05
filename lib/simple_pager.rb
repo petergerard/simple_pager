35e0f22fd3cee6d27c237e5cda0d062dfb636222
Peter Gerard <peter@accidental.tv>
2012-10-06 00:42:45 +0100

    update

--- lib/simple_pager.rb
+++ lib/simple_pager.rb
@@ -1,25 +1,21 @@
 require 'active_record'
 module SimplePager
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
   # = Simple Paginating for ActiveRecord models
   # 
   # Adds pager scope to all ActiveRecord models. Works similar to will_paginate but does not count the collection.
-  # You can't set a universal per_page yet.
+  # You can't set a universal per_page yet, but you can set it on the model. Defaults to 30.
   #
-  #   @posts = Post.pager :all, :page => params[:page], :order => 'created_at DESC'
-  #
-  module ActiveRecord
-    # makes a Relation look like WillPaginate::Collection
-    module Pager
-      def pager(pp)
-        limit(pp[:per_page] || (self.respond_to?(:per_page) ? self.per_page : 30)).
-        offset(pp[:page].blank? ? 0 : ((pp[:page].to_i-1)*(pp[:per_page] || (self.respond_to?(:per_page) ? self.per_page : 30)))) 
-      end
+  #   @posts = Post.pager(:page => params[:page]).order('created_at DESC')
+  module ClassMethods
+    def pager(pp={})
+      limit(pp[:per_page] || (self.respond_to?(:per_page) ? self.per_page : 30)).
+      offset(pp[:page].blank? ? 0 : ((pp[:page].to_i-1)*(pp[:per_page] || (self.respond_to?(:per_page) ? self.per_page : 30)))) 
     end
-    
-    # mix everything into Active Record
-    ::ActiveRecord::Base.extend Pager
-
-    # support pagination on associations and scopes
-    klasses.each { |klass| klass.send(:include, Pager) }
-  end       
+  end  
 end
+
+# mix everything into Active Record
+ActiveRecord::Base.send(:include, SimplePager)
