194be29f08ec6cf919de502ae354561f816bafc4
Peter Gerard <peter@accidental.tv>
2012-10-06 00:47:16 +0100

    helper

--- lib/simple_pager.rb
+++ lib/simple_pager.rb
@@ -17,5 +17,24 @@ module SimplePager
   end  
 end
 
-# mix everything into Active Record
+module SimplePagerHelper
+  def simple_pager(collection,per_page=nil,prev_name='Previous',next_name='More')
+    return if !collection
+    per_page = collection.first.class.per_page if per_page.blank? and collection.first.class.respond_to?(:per_page)
+    per_page ||= 30
+    pprev = ''
+    pnext = ''
+    if params[:page] and params[:page].to_i > 1
+      pprev = content_tag(:li, link_to( "&larr; #{prev_name}".html_safe, request.query_parameters.merge({:page => (params[:page].to_i - 1)}) ),:class => 'previous')
+    end
+    if !@no_more_pages and collection.size >= per_page
+      pnext = content_tag(:li, link_to( "#{next_name} &rarr;".html_safe, request.query_parameters.merge({:page => (params[:page] ? params[:page].to_i + 1 : 2)}) ),:class => 'next')
+    end
+    content_tag(:ul, (pprev+pnext).html_safe, :class => 'pager')
+  end
+end
+
+
+# mix everything into rails
 ActiveRecord::Base.send(:include, SimplePager)
+ActionView::Base.send( :include, SimplePagerHelper)
